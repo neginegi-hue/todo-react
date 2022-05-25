c74a99e60156054168136bacdaf12a1a2846bc9e
neginegi-hue <105393179+neginegi-hue@users.noreply.github.com>
2022-05-25 22:42:18 +0900

    divのコンポーネントを作った

---
+++ src/components/completeTodos.jsx
@@ -0,0 +1,18 @@
+export const CompleteTodos = (props) => {
+  const { todos, onClickIncomplete } = props;
+  return (
+    <div className="complete-area">
+      <p className="title">完了のTODO</p>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClickIncomplete(index)}>戻す</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import ReactDom from "react-dom";
 
 import { App } from "./App";
